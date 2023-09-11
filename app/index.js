3ebd3f3cbd9a23b49faea927cf5cbc2a00989ee5
Niko Malm <nicodude00@gmail.com>
2023-09-11 13:18:41 +0300

    project base

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 relative">
+        <StatusBar style="dark" />
+        <Text>Lets start working on our app.
+          
+        </Text>
+    </View>
+    
+   
+      
+  )
+}
+
+const styles = StyleSheet.create({
+  
+});
